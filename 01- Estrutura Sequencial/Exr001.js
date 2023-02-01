c39498ac788cf018d99933e200d8261a22974547
Pedro <pedroflopes1710@gmail.com>
2023-02-01 00:09:42 -0300

    tópicos de estudos

---
+++ 01- Estrutura Sequencial/Exr001.js
@@ -0,0 +1,15 @@
+// Faça um programa que receba quatro números inteiros, calcule e mostre a soma desses números.
+
+"use strict";
+
+const ps = require("prompt-sync");
+const prompt = ps();
+
+let num1 = parseInt(prompt("Digite o primeiro número:"));
+let num2 = parseInt(prompt("Digite o segundo número:"));
+let num3 = parseInt(prompt("Digite o terceiro número:"));
+let num4 = parseInt(prompt("Digite o quarto número:"));
+
+let soma = num1 + num2 + num3 + num4;
+
+console.log("A soma dos números é: " + soma);
